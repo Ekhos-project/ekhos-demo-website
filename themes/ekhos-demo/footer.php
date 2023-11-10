238186ed9e6fdc36754f0e80a2afc4337b2e76f9
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-11-10 15:43:03 +0100

    feat: footer section

--- themes/ekhos-demo/footer.php
+++ themes/ekhos-demo/footer.php
@@ -4,8 +4,21 @@ wp_footer();
 </div>
 
 <footer>
+    <div class="footer_container">
+        <div class="footer_title">
+            <h2>Il ne reste qu’une seule chose à faire…</h2>
+        </div>
+
+        <div class="footer_action">
+            <a class="button endicon" href="">Faire un devis</a>
+        </div>
+
+        <div class="footer_logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/footer_logo_image.png" alt="logo">
+        </div>
+    </div>
 </footer>
 
-<script src=<?php echo get_template_directory_uri(); ?>/assets/scripts/script.js" type="module"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/scripts/script.js" type="module"></script>
 </body>
 </html>
