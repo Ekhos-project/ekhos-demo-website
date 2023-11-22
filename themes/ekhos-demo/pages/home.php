580322207384b95a8add666a55e07b308c1209ca
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2023-11-22 15:31:47 +0100

    feat: home sections

--- themes/ekhos-demo/pages/home.php
+++ themes/ekhos-demo/pages/home.php
@@ -12,3 +12,43 @@
         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home_hero_background.png" alt="background">
     </div>
 </section>
+
+<section class="section section-home" id="product">
+    <div class="product_container">
+        <div class="product_text">
+            <h2>Ceci est votre produit</h2>
+            <p>C’est ici que tout commence ! Depuis un storyboard et votre storytelling, créer votre histoire pour mettre en avant votre produit. Votre son et votre voix vous accompagnera tous le long de votre aventure !</p>
+        </div>
+
+        <div class="product_visual">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home_product_image.png" alt="visual">
+        </div>
+    </div>
+</section>
+
+<section class="section section-home" id="sound">
+    <div class="sound_container">
+        <div class="sound_text">
+            <h2>À travers le son et la voix</h2>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+
+        <div class="sound_visual">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home_sound_image.png" alt="visual">
+        </div>
+    </div>
+</section>
+
+<section class="section section-home" id="story">
+    <div class="story_container">
+        <div class="story_text">
+            <h2>Votre histoire, un chemin</h2>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <i class="story_action"></i>
+        </div>
+
+        <div class="story_visual">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/home_story_image.png" alt="visual">
+        </div>
+    </div>
+</section>
